7b174654e9c1eeb8beacab354d1171a50975fa17
simidai <sdaikis2000@gmail.com>
2025-09-30 17:11:40 +0900

    change main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
 import JsonInputDisplay from './JsonInputDisplay';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router basename="/maitool">
-      <JsonInputDisplay />
-    </Router>
+    <JsonInputDisplay />
   </React.StrictMode>
 );
